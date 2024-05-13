3a4c75d8e903d714baab2ea444bfefa568897079
Bogdan Bulakh <bogdan.bulah@gmail.com>
2024-05-13 15:02:55 +0300

    fix: components dependencies

--- app/entrypoints/renderer/src/app/page.tsx
+++ app/entrypoints/renderer/src/app/page.tsx
@@ -1,20 +1,30 @@
-"use client"
+'use client'
 
-import React from 'react'
+import React             from 'react'
 
-import {Layout} from '@ui/layout'
-import {Box} from '@ui/layout'
-import {Circle} from '@ui/progress'
+import { Divider }       from '@ui/divider'
+import { Layout }        from '@ui/layout'
+import { Box }           from '@ui/layout'
+import { Circle }        from '@ui/progress'
+import { ThemeProvider } from '@ui/theme'
 
-const Page = () => {
-    return <>
-        <Circle percent={25}/>
-        <Layout>
-            <Box>
-                <p>Hello</p>
-            </Box>
-        </Layout>
-    </>
-}
+const Page = () => (
+  <ThemeProvider>
+    <Circle percent={25} />
+    <Layout>
+      <Box flexDirection='column'>
+        <div>
+          <p>Apple</p>
+        </div>
+        <Divider weight={10} color='gray_200' />
+        <p>Banana</p>
+        <Divider width={10} height={10} backgroundColor='gray' />
+        <p>Grapefruit</p>
+        <Divider width={10} height={10} backgroundColor='gray' />
+        <p>Peach</p>
+      </Box>
+    </Layout>
+  </ThemeProvider>
+)
 
 export default Page
