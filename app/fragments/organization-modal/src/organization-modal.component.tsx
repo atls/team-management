e69c5278614d65fc7dea4fbe88717cf178d1a27f
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-24 14:49:18 +0300

    feat(organization-modal): pseudo layout

--- app/fragments/organization-modal/src/organization-modal.component.tsx
+++ app/fragments/organization-modal/src/organization-modal.component.tsx
@@ -5,5 +5,28 @@ import { PropsWithChildren }      from 'react'
 import { OrganizationModalProps } from './organization-modal.interfaces'
 
 export const OrganizationModal: FC<PropsWithChildren<OrganizationModalProps>> = () => (
-  <>organization modal</>
+  <div>
+    <h1>Добавить сотрудника в организацию</h1>
+    <h1>input</h1>
+    <div>
+      <h1>Команды организации</h1>
+      <div>
+        <h1>teamName</h1>
+        <h1>switch</h1>
+      </div>
+      <div>
+        <h1>teamName</h1>
+        <h1>switch</h1>
+      </div>
+      <div>
+        <h1>teamName</h1>
+        <h1>switch</h1>
+      </div>
+      <div>
+        <h1>teamName</h1>
+        <h1>switch</h1>
+      </div>
+    </div>
+    <h1>button</h1>
+  </div>
 )
