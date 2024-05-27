7bb4d40adc3061a44b4fb443efad9013c84baf58
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-27 13:33:16 +0300

    feat(layout): add rowGap and columnGap props

--- ui/layout/src/styles/index.ts
+++ ui/layout/src/styles/index.ts
@@ -4,11 +4,21 @@ import { system }        from 'styled-system'
 
 export interface LayoutWithGap {
   gap?: CSSProperties['gap']
+  rowGap?: CSSProperties['rowGap']
+  columnGap?: CSSProperties['columnGap']
 }
 
 const gapConfig: Config = {
   gap: {
     property: 'gap',
+    transform: (value) => (typeof value === 'number' ? `${value}px` : value),
+  },
+  rowGap: {
+    property: 'rowGap',
+    transform: (value) => (typeof value === 'number' ? `${value}px` : value),
+  },
+  columnGap: {
+    property: 'columnGap',
     transform: (value) => (typeof value === 'number' ? `${value}px` : value),
   },
 }
