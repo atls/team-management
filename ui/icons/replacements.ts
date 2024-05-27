fd6f2b8010e5e3af61ce582107e7cc160a6d489c
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-27 16:58:54 +0300

    fix(icons): add color props to some icons, who needed for switch

---
+++ ui/icons/replacements.ts
@@ -0,0 +1,14 @@
+export const replacements = {
+  GitHubIcon: {
+    '#282828': '{props.color || "#282828"}',
+  },
+  FigmaIcon: {
+    '#282828': '{props.color || "#282828"}',
+  },
+  DiscordIcon: {
+    '#282828': '{props.color || "#282828"}',
+  },
+  TelegramIcon: {
+    '#282828': '{props.color || "#282828"}',
+  },
+}
