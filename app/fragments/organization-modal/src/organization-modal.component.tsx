ce8edbe4e5747727fe3e9fd89da650b90d55edf9
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-28 16:34:41 +0300

    fix(fragment.orgranization-modal): fix theme props

--- app/fragments/organization-modal/src/organization-modal.component.tsx
+++ app/fragments/organization-modal/src/organization-modal.component.tsx
@@ -1,6 +1,7 @@
+import { useTheme }               from '@emotion/react'
+
 import React                      from 'react'
 import { FC }                     from 'react'
-import { PropsWithChildren }      from 'react'
 
 import { Button }                 from '@ui/button'
 import { Input }                  from '@ui/input'
@@ -12,40 +13,44 @@ import { Text }                   from '@ui/text'
 import { OrganizationModalProps } from './organization-modal.interfaces'
 import { TeamSwitch }             from './team-switch'
 
-export const OrganizationModal: FC<PropsWithChildren<OrganizationModalProps>> = ({ open }) => (
-  <Modal open={open} width={578}>
-    <Column flexDirection='column' gap={24}>
-      <Text fontSize='medium.semiReduced' fontWeight='normal' padding={12}>
-        Добавить сотрудника в организацию
-      </Text>
-      <Input placeholder='Team member' />
-      <Row
-        flexDirection='row'
-        flexWrap='wrap'
-        padding={12}
-        justifyContent='space-between'
-        rowGap={24}
-      >
-        <Text fontSize='normal.semiIncreased' width='100%'>
-          Команды организации
+export const OrganizationModal: FC<OrganizationModalProps> = ({ open }) => {
+  const theme: any = useTheme()
+
+  return (
+    <Modal open={open} width={theme.spaces.superPuperExtra}>
+      <Column flexDirection='column' gap={theme.spaces.large}>
+        <Text fontSize='medium.semiReduced' fontWeight='normal' padding={theme.spaces.micro}>
+          Добавить сотрудника в организацию
         </Text>
-        <TeamSwitch teamName='Design' />
-        <TeamSwitch teamName='Frontend' />
-        <TeamSwitch teamName='Backend' />
-        <TeamSwitch teamName={`Guest's`} />
-      </Row>
-      <Row justifyContent='end'>
-        <Button
-          disabled
-          horizontalLocation='right'
-          variant='blueBackgroundButton'
-          size='middlingRoundedPadding'
+        <Input placeholder='Team member' />
+        <Row
+          flexDirection='row'
+          flexWrap='wrap'
+          padding={theme.spaces.micro}
+          justifyContent='space-between'
+          rowGap={theme.spaces.large}
         >
-          <Text fontSize='normal.semiDefault' fontWeight='normal'>
-            Пригласить
+          <Text fontSize='normal.semiIncreased' width='100%'>
+            Команды организации
           </Text>
-        </Button>
-      </Row>
-    </Column>
-  </Modal>
-)
+          <TeamSwitch teamName='Design' />
+          <TeamSwitch teamName='Frontend' />
+          <TeamSwitch teamName='Backend' />
+          <TeamSwitch teamName={`Guest's`} />
+        </Row>
+        <Row justifyContent='end'>
+          <Button
+            disabled
+            horizontalLocation='right'
+            variant='blueBackgroundButton'
+            size='middlingRoundedPadding'
+          >
+            <Text fontSize='normal.semiDefault' fontWeight='normal'>
+              Пригласить
+            </Text>
+          </Button>
+        </Row>
+      </Column>
+    </Modal>
+  )
+}
