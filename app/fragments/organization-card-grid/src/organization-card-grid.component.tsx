9949c67de4874e0d5c577040fb4de8b8e270b1b0
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-30 20:43:57 +0300

    fix(organization-card-grid): use new grid-auto-rows component

--- app/fragments/organization-card-grid/src/organization-card-grid.component.tsx
+++ app/fragments/organization-card-grid/src/organization-card-grid.component.tsx
@@ -1,16 +1,12 @@
-import styled                        from '@emotion/styled'
-
 import React                         from 'react'
 import { FC }                        from 'react'
 
-import { Box }                       from '@ui/layout'
+import { GridAutoRows }              from '@ui/layout'
 
 import { OrganizationCardGridProps } from './organization-card-grid.interfaces'
-import { baseGridContainerStyles }   from './organization-card-grid.styles'
-import { shapeGridContainerStyles }  from './organization-card-grid.styles'
-
-const GridContainer = styled(Box)(baseGridContainerStyles, shapeGridContainerStyles)
 
 export const OrganizationCardGrid: FC<OrganizationCardGridProps> = ({ children }) => (
-  <GridContainer>{children}</GridContainer>
+  <GridAutoRows rowHeight={150} columns={2} maxColumnWidth={620}>
+    {children}
+  </GridAutoRows>
 )
