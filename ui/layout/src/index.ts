3dd44440bbeaa108adc0fab8bc0d495bd81b20c0
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-01 18:37:21 +0300

    fix(layout.component): re-exports box and layout from hyperion

--- ui/layout/src/index.ts
+++ ui/layout/src/index.ts
@@ -1,4 +1,11 @@
-export { Box } from '@atls-ui-parts/layout'
-export { Layout } from '@atls-ui-parts/layout'
+import styled                   from '@emotion/styled'
+import { Layout as BaseLayout } from '@atls-ui-parts/layout'
+import { Box as BaseBox }       from '@atls-ui-parts/layout'
+
+import { flexStyles }           from './flex/flex.styles'
+
+export const Box = styled(BaseBox)(flexStyles)
+export const Layout = styled(BaseLayout)(flexStyles)
+
 export * from './flex'
 export * from './grid'
