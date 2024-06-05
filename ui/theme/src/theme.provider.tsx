8aa41f9134f535073ded14b3b30225faccf65023
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-05 13:23:50 +0300

    feat(all package.json): type module

--- ui/theme/src/theme.provider.tsx
+++ ui/theme/src/theme.provider.tsx
@@ -1,10 +1,35 @@
-import * as theme                                from './theme'
-
 import { ThemeProvider as EmotionThemeProvider } from '@emotion/react'
 
 import React                                     from 'react'
 
 import { GlobalStyles }                          from './global.styles'
+import { backgrounds }                           from './theme'
+import { borders }                               from './theme'
+import { colors }                                from './theme'
+import { fonts }                                 from './theme'
+import { fontSizes }                             from './theme'
+import { fontWeights }                           from './theme'
+import { lineHeights }                           from './theme'
+import { radii }                                 from './theme'
+import { shadows }                               from './theme'
+import { space }                                 from './theme'
+import { spaces }                                from './theme'
+
+const theme = {
+  backgrounds,
+  borders,
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  shadows,
+  space,
+  spaces,
+}
+
+export type ThemeType = typeof theme
 
 export const ThemeProvider = ({ children }) => (
   <>
