21e3f44396da55ea3d0dc0b2a51b34e00e26dcd3
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-06 15:37:28 +0300

    fix: chekcs

--- app/fragments/add-member-modal/src/input/input.interfaces.ts
+++ app/fragments/add-member-modal/src/input/input.interfaces.ts
@@ -1,8 +1,11 @@
-import { InputProps } from '@ui/input'
+import { InputProps }      from '@ui/input'
+
+import { InputValuesType } from '../add-member-modal.interfaces.js'
 
 export interface AddMemberModalInputProps extends InputProps {
   inputIndex: number
-  updateInputValuesHook: Function
+  inputValues: InputValuesType
+  setInputValues: Function
 }
 export type HandleInputChangeType = (e: React.ChangeEvent<HTMLInputElement>) => void
 export type HandleInputDeleteType = () => void
