ae63d4650e183f84cb7ab129282864dde5621a1d
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-10 18:06:12 +0300

    feat(org-page): base layout done; provide components; render some cards; start writing test-data object

--- app/pages/organizations-page/src/organizations.page.tsx
+++ app/pages/organizations-page/src/organizations.page.tsx
@@ -1,14 +1,36 @@
-import React   from 'react'
-import { FC }  from 'react'
+import React                    from 'react'
+import { FC }                   from 'react'
 
-import { Row } from '@ui/layout'
-import { Box } from '@ui/layout'
+import { OrganizationCard }     from '@fragments/organization-card'
+import { OrganizationCardGrid } from '@fragments/organization-card-grid'
+import { Row }                  from '@ui/layout'
+import { Column }               from '@ui/layout'
+import { Box }                  from '@ui/layout'
+import { Sidebar }              from '@ui/sidebar'
 
 export const OrganizationsPage: FC<any> = ({}) => {
+  const TEST_ORGANIZATION_DATA = {}
+
+  const TEST_ORGANIZATIONS_DATA = [
+    TEST_ORGANIZATION_DATA,
+    TEST_ORGANIZATION_DATA,
+    TEST_ORGANIZATION_DATA,
+    TEST_ORGANIZATION_DATA,
+  ]
+
+  const organizationsData = TEST_ORGANIZATIONS_DATA
+
   return (
     <Row>
-      <Box>sidebar</Box>
-      <Box>grid</Box>
+      <Box>SideBar</Box>
+      <Column width='100%' justifyContent='center' paddingTop={110}>
+        <OrganizationCardGrid>
+          <Box gridColumn='1 / -1'>Team members</Box>
+          {organizationsData.map((organizationData) => (
+            <OrganizationCard {...organizationData} />
+          ))}
+        </OrganizationCardGrid>
+      </Column>
     </Row>
   )
 }
