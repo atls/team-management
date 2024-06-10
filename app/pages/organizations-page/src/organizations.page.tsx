fe35b58a27a4a04f413b5b87a12150046b29cb78
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-10 12:18:19 +0300

    feat(organizations-page): pseudo

--- app/pages/organizations-page/src/organizations.page.tsx
+++ app/pages/organizations-page/src/organizations.page.tsx
@@ -1,6 +1,14 @@
-import React  from 'react'
-import { FC } from 'react'
+import React   from 'react'
+import { FC }  from 'react'
+
+import { Row } from '@ui/layout'
+import { Box } from '@ui/layout'
 
 export const OrganizationsPage: FC<any> = ({}) => {
-  return <>OrganizationsPage</>
+  return (
+    <Row>
+      <Box>sidebar</Box>
+      <Box>grid</Box>
+    </Row>
+  )
 }
