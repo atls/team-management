e341a0b0e1e856ecb204d0e86373bd473b8c9865
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-10 18:07:30 +0300

    feat(global-styles): reset styles (copy from drum-in)

--- ui/theme/src/global.styles.tsx
+++ ui/theme/src/global.styles.tsx
@@ -6,7 +6,27 @@ import React      from 'react'
 export const GlobalStyles = () => (
   <Global
     styles={css`
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap');
-      `}
+      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap');
+
+      html,
+      body,
+      #__next {
+        margin: 0;
+        -webkit-font-smoothing: antialiased;
+        -webkit-overflow-scrolling: touch;
+        height: 100%;
+        scroll-behavior: smooth;
+      }
+
+      html,
+      body {
+        overflow-x: hidden;
+      }
+
+      #__next {
+        display: flex;
+        flex-direction: column;
+      }
+    `}
   />
 )
