8e3e805d44609e1b1279ca659bb35c7eff3a7d67
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-11 15:57:36 +0300

    feat(organizations-page): render cards with testData; cards onl, theme switch

--- app/pages/organizations-page/src/organizations.page.tsx
+++ app/pages/organizations-page/src/organizations.page.tsx
@@ -1,6 +1,8 @@
 import React                    from 'react'
 import { FC }                   from 'react'
 
+import { ThemeSelector }        from '@app/theme-selector'
+import { UsersTitle }           from '@app/users-title'
 import { OrganizationCard }     from '@fragments/organization-card'
 import { OrganizationCardGrid } from '@fragments/organization-card-grid'
 import { Row }                  from '@ui/layout'
@@ -9,7 +11,14 @@ import { Box }                  from '@ui/layout'
 import { Sidebar }              from '@ui/sidebar'
 
 export const OrganizationsPage: FC<any> = ({}) => {
-  const TEST_ORGANIZATION_DATA = {}
+  const TEST_ORGANIZATION_DATA = {
+    organizationId: 1,
+    organizationTitle: 'atls',
+    organizationDescription: 'Atlantis Main Team',
+    organizationMembersQuantity: 15,
+    organizationCoverSrc:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.sWCvltMZF_s3mjA5sL-RdgHaE8%26pid%3DApi&f=1&ipt=75b1307f72623776b37217b88a805f10036ed22338715def57bb6eeb6c55323b&ipo=images',
+  }
 
   const TEST_ORGANIZATIONS_DATA = [
     TEST_ORGANIZATION_DATA,
@@ -22,10 +31,13 @@ export const OrganizationsPage: FC<any> = ({}) => {
 
   return (
     <Row>
+      <ThemeSelector />
       <Box>SideBar</Box>
       <Column width='100%' justifyContent='center' paddingTop={110}>
         <OrganizationCardGrid>
-          <Box gridColumn='1 / -1'>Team members</Box>
+          <Box gridColumn='1 / -1'>
+            <UsersTitle />
+          </Box>
           {organizationsData.map((organizationData) => (
             <OrganizationCard {...organizationData} />
           ))}
