57fc32488a7ba8104582e0b53721bd21d40bc906
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-11 18:06:06 +0300

    feat(org-card.fragment): show modals on button; handlers to backdrop

--- app/fragments/organization-card/src/organization-card.component.tsx
+++ app/fragments/organization-card/src/organization-card.component.tsx
@@ -1,30 +1,46 @@
-import { useTheme }              from '@emotion/react'
+import { useTheme }                     from '@emotion/react'
 
-import React                     from 'react'
-import { FC }                    from 'react'
-import { memo }                  from 'react'
+import React                            from 'react'
+import { FC }                           from 'react'
+import { memo }                         from 'react'
+import { useState }                     from 'react'
 
-import { Button }                from '@ui/button'
-import { TeamIcon }              from '@ui/icons'
-import { AddIcon }               from '@ui/icons'
-import { ImageBlock }            from '@ui/image'
-import { Row }                   from '@ui/layout'
-import { Column }                from '@ui/layout'
-import { Box }                   from '@ui/layout'
-import { Text }                  from '@ui/text'
-import { ThemeType }             from '@ui/theme'
+import { UsersModal }                   from '@app/users-modal'
+import { AddMemberToOrganizationModal } from '@fragments/add-member-to-organization-modal'
+import { Button }                       from '@ui/button'
+import { TeamIcon }                     from '@ui/icons'
+import { AddIcon }                      from '@ui/icons'
+import { ImageBlock }                   from '@ui/image'
+import { Row }                          from '@ui/layout'
+import { Column }                       from '@ui/layout'
+import { Box }                          from '@ui/layout'
+import { Text }                         from '@ui/text'
+import { ThemeType }                    from '@ui/theme'
 
-import { Members }               from './members/index.js'
-import { OrganizationCardProps } from './organization-card.interfaces.js'
+import { Members }                      from './members/index.js'
+import { OrganizationCardProps }        from './organization-card.interfaces.js'
 
 export const OrganizationCard: FC<OrganizationCardProps> = memo(({
-  organizatoinId,
+  organizationId,
   organizationTitle,
   organizationDescription,
   organizationMembersQuantity,
   organizationCoverSrc,
 }) => {
+  const [isAddMemberToOrganizationModalOpen, setAddMemberToOrganizationModalOpen] =
+    useState<boolean>(false)
+  const [isUsersModalOpen, setUsersModalOpen] = useState<boolean>(false)
+
   const theme = useTheme() as ThemeType
+
+  const handlerAddOrganizationMemberClick = () => {
+    setAddMemberToOrganizationModalOpen(!isAddMemberToOrganizationModalOpen)
+  }
+
+  const handlerUsersModalClick = () => {
+    console.log('click')
+    setUsersModalOpen(!isUsersModalOpen)
+  }
 
   return (
     <Box
@@ -53,20 +69,39 @@ export const OrganizationCard: FC<OrganizationCardProps> = memo(({
             </Row>
           </Column>
           <Row gap={theme.spaces.micro} justifyContent='end'>
-            <Button shape='circle' size='middlingRoundedPadding' variant='blueBackgroundButton'>
+            <Button
+              shape='circle'
+              size='middlingRoundedPadding'
+              variant='blueBackgroundButton'
+              onClick={handlerUsersModalClick}
+            >
               <TeamIcon
                 color='white'
                 width={theme.spaces.semiRegular}
                 height={theme.spaces.semiRegular}
               />
             </Button>
-            <Button shape='circle' size='middlingRoundedPadding' variant='blueBackgroundButton'>
+
+            <UsersModal open={isUsersModalOpen} onBackdropClick={handlerUsersModalClick} />
+
+            <Button
+              shape='circle'
+              size='middlingRoundedPadding'
+              variant='blueBackgroundButton'
+              onClick={handlerAddOrganizationMemberClick}
+            >
               <AddIcon
                 color='white'
                 width={theme.spaces.semiRegular}
                 height={theme.spaces.semiRegular}
               />
             </Button>
+
+            <AddMemberToOrganizationModal
+              organizationId={organizationId}
+              open={isAddMemberToOrganizationModalOpen}
+              onBackdropClick={handlerAddOrganizationMemberClick}
+            />
           </Row>
         </Column>
       </Row>
