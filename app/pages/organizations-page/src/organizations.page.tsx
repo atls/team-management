8012fa39362c7203d2ce38ed645648cdf410dd60
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-12 15:59:27 +0300

    fix(organizations.page): move baseLayout components out

--- app/pages/organizations-page/src/organizations.page.tsx
+++ app/pages/organizations-page/src/organizations.page.tsx
@@ -1,14 +1,11 @@
 import React                    from 'react'
 import { FC }                   from 'react'
 
-import { ThemeSelector }        from '@app/theme-selector'
 import { UsersTitle }           from '@app/users-title'
 import { OrganizationCard }     from '@fragments/organization-card'
 import { OrganizationCardGrid } from '@fragments/organization-card-grid'
-import { Row }                  from '@ui/layout'
 import { Column }               from '@ui/layout'
 import { Box }                  from '@ui/layout'
-import { Sidebar }              from '@ui/sidebar'
 
 export const OrganizationsPage: FC<any> = ({}) => {
   const ORGANIZATION_TEST_DATA = {
@@ -30,19 +27,15 @@ export const OrganizationsPage: FC<any> = ({}) => {
   const organizationsData = TEST_ORGANIZATIONS_DATA
 
   return (
-    <Row>
-      <ThemeSelector />
-      <Box>SideBar</Box>
-      <Column width='100%' justifyContent='center' paddingTop={110}>
-        <OrganizationCardGrid>
-          <Box gridColumn='1 / -1'>
-            <UsersTitle />
-          </Box>
-          {organizationsData.map((organizationData) => (
-            <OrganizationCard {...organizationData} />
-          ))}
-        </OrganizationCardGrid>
-      </Column>
-    </Row>
+    <Column width='100%' justifyContent='center' paddingTop={110}>
+      <OrganizationCardGrid>
+        <Box gridColumn='1 / -1'>
+          <UsersTitle />
+        </Box>
+        {organizationsData.map((organizationData) => (
+          <OrganizationCard {...organizationData} />
+        ))}
+      </OrganizationCardGrid>
+    </Column>
   )
 }
