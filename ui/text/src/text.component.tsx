cfa261bda2d1df027d2d43165a0629e013cc8349
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-13 18:51:16 +0300

    feat(text) add primary and secondary colors; depends on current theme

---
+++ ui/text/src/text.component.tsx
@@ -0,0 +1,13 @@
+import { Text as BaseText } from '@atls-ui-parts/text'
+import { useTheme }         from '@emotion/react'
+
+import React                from 'react'
+import { FC }               from 'react'
+
+import { ThemeType }        from '@ui/theme'
+
+// TODO interface
+export const Text: FC<any> = (props) => {
+  const theme = useTheme() as ThemeType
+  return <BaseText color={props.color || theme.colors.text.primary} {...props} />
+}
