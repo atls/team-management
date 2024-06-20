908897af9b185c3f8ac13fbc1531564cc5b468ef
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-20 20:06:21 +0300

    fix(registration.server): write gh tokkent to cookies

--- app/fragments/app-registration/src/registration.server.ts
+++ app/fragments/app-registration/src/registration.server.ts
@@ -1,13 +1,16 @@
+import { getSidebarUserDataHook } from '@globals/data'
+
 // @ts-ignore:next-line
-import { cookies }  from 'next/headers'
+import { cookies }                from 'next/headers'
 // @ts-ignore:next-line
-import { redirect } from 'next/navigation'
+import { redirect }               from 'next/navigation'
 
 export async function RegistrationServerGet(request: Request) {
   const { searchParams } = new URL(request.url)
 
   const code = searchParams.get('code')
-  if (code) cookies().set('code', code)
+  const token = await getGithubAccessToken(code)
+  if (token) cookies().set('token', token)
 
   const redirectUrl = new URL(request.url)
   return redirect(redirectUrl.origin)
