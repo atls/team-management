4b92ca5acbfcb3ff1a8beb46fcfe1036aab27aaf
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-21 12:28:31 +0300

    draft: authorization; scopes

--- app/fragments/app-base-layout/src/base-layout.server.ts
+++ app/fragments/app-base-layout/src/base-layout.server.ts
@@ -4,25 +4,34 @@ import { cookies } from 'next/headers'
 
 export const BaseLayoutServer = async () => {
   const token = cookies().get('token').value
-
-  console.log(token)
+  // TODO redirect to registration if token not exist
 
   const graphqlWithAuth = graphql.defaults({
     headers: {
       authorization: `token ${token}`,
     },
   })
-  const { repository } = await graphqlWithAuth(`
-  {
-    repository(owner: "octokit", name: "graphql.js") {
-      issues(last: 3) {
-        edges {
-          node {
-            title
-          }
-        }
-      }
-    }
-  }
-`)
+
+  //   const response = await graphqlWithAuth(`
+  //   {
+  //     repository(owner: "octokit", name: "graphql.js") {
+  //       issues(last: 3) {
+  //         edges {
+  //           node {
+  //             title
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+  // TODO query in query
+  const response = await graphqlWithAuth(`
+	  {
+	    viewer { login, name, email, avatarUrl }
+	  }
+	`)
+
+  console.log(response)
 }
--- app/fragments/app-registration/src/_registration.server.ts
+++ app/fragments/app-registration/src/_registration.server.ts
@@ -10,31 +10,48 @@ export async function RegistrationServerGet(request: Request) {
 
   const code = searchParams.get('code')
 
+  if (!code) {
+    throw new Error('auth error; code not recived from github')
+  }
+
+  // env checks on root layout
   const getPrimaryGithubToken = async (code) => {
     const auth = createOAuthUserAuth({
       clientId: process.env.NEXT_PUBLIC_GH_CLIENT_ID as string,
       clientSecret: process.env.NEXT_PUBLIC_GH_CLIENT_SECRET as string,
       code,
     })
+
     const { token } = await auth()
     return token
   }
+
   const primaryGithubToken = await getPrimaryGithubToken(code)
 
+  console.log('p: ', primaryGithubToken)
+
+  if (!primaryGithubToken) {
+    throw new Error('auth error; token not recived from octokit auth func')
+  }
+
   const getSecondaryGithubToken = async (primaryGithubToken) => {
-    // TODO scopes in constant
     const auth = createOAuthUserAuth({
       clientType: 'oauth-app',
       clientId: process.env.NEXT_PUBLIC_GH_CLIENT_ID as string,
       clientSecret: process.env.NEXT_PUBLIC_GH_CLIENT_SECRET as string,
       token: primaryGithubToken,
-      scopes: ['read:user', 'user:email'],
+      scopes: ['user:email', 'read:user'],
     })
+
     const authObject = await auth()
+    console.log(authObject.scopes)
     const { token } = authObject
     return token
   }
+
   const secondaryGithubToken = await getSecondaryGithubToken(primaryGithubToken)
+
+  console.log('s: ', secondaryGithubToken)
 
   cookies().set('token', secondaryGithubToken)
 
