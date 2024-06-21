0404cc7a73005aff9e2aa3260a78bcbc83c0774c
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-21 20:34:35 +0300

    fix(gh-app-server-component): fix private key auth method

--- globals/github-server-app/src/github-server-app.server-component.ts
+++ globals/github-server-app/src/github-server-app.server-component.ts
@@ -1,5 +1,4 @@
-import { GH_APP_PRIVATE_KEY } from '@globals/privates'
-import { App }                from '@octokit/app'
+import { App } from '@octokit/app'
 
 // по моей логике - этот компонент можно переиспользовать в разных частях приложения.
 // мы проводим авторизацию от лица GithubApp один раз при запуске приложения
@@ -13,10 +12,10 @@ import { App }                from '@octokit/app'
 class GithubServerAppClass {
   constructor() {}
 
-  authenticate() {
+  authenticate(privateKey) {
     this.app = new App({
       appId: process.env.GH_APP_ID,
-      privateKey: GH_APP_PRIVATE_KEY,
+      privateKey,
       oauth: {
         clientId: process.env.NEXT_PUBLIC_GH_CLIENT_ID as string,
         clientSecret: process.env.NEXT_PUBLIC_GH_CLIENT_SECRET as string,
