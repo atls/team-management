6ad63f0848906feefa70c8f2c1ba16d22d537d05
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-21 20:38:43 +0300

    fix(root-layout): moves all logic to fragment

--- app/entrypoints/renderer/src/app/layout.tsx
+++ app/entrypoints/renderer/src/app/layout.tsx
@@ -1,9 +1,1 @@
-import { RootLayoutClient } from './layout.client.js'
-import { RootLayoutServer } from './layout.server.js'
-
-const RootLayout = async ({ children }) => {
-  await RootLayoutServer()
-  return <RootLayoutClient>{children}</RootLayoutClient>
-}
-
-export default RootLayout
+export { default } from '@app/root-layout'
