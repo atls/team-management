cdd526e30d21f517ce384cea4a312a3066d472b2
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-21 08:37:32 +0300

    fix(registration.server): move octokit auth func from @globals to registration.server

--- app/fragments/app-registration/src/registration.server.ts
+++ app/fragments/app-registration/src/registration.server.ts
@@ -1,16 +1,33 @@
-import { getSidebarUserDataHook } from '@globals/data'
+import { createOAuthUserAuth } from '@octokit/auth-oauth-user'
 
 // @ts-ignore:next-line
-import { cookies }                from 'next/headers'
+import { cookies }             from 'next/headers'
 // @ts-ignore:next-line
-import { redirect }               from 'next/navigation'
+import { redirect }            from 'next/navigation'
 
 export async function RegistrationServerGet(request: Request) {
   const { searchParams } = new URL(request.url)
 
   const code = searchParams.get('code')
-  const token = await getGithubAccessToken(code)
+
+  let token: string
+
+  if (code) {
+    const auth = createOAuthUserAuth({
+      clientId: process.env.NEXT_PUBLIC_GH_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GH_CLIENT_SECRET,
+      code,
+    })
+
+    const authResult = await auth()
+    token = authResult.token
+  } else {
+    throw new Error('auth error')
+  }
+
   if (token) cookies().set('token', token)
+  // TODO locales
+  else throw new Error('auth error')
 
   const redirectUrl = new URL(request.url)
   return redirect(redirectUrl.origin)
