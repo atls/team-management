db432d87931895fd081c1190562580ce7b11bcd6
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-22 13:13:05 +0300

    fix(base-layout): move data logic to globals data

--- app/fragments/app-base-layout/src/base-layout.server.ts
+++ app/fragments/app-base-layout/src/base-layout.server.ts
@@ -1,28 +1,14 @@
-import { graphql } from '@octokit/graphql'
+import { GET_VIEWER }           from '@globals/data'
+import { octokitGraphqlClient } from '@globals/data'
 
-import { cookies } from 'next/headers'
+import { cookies }              from 'next/headers'
 
 export const BaseLayoutServer = async () => {
   const token = cookies().get('token').value
   // TODO redirect to registration if token not exist
   // TODO middleware
 
-  const graphqlWithAuth = graphql.defaults({
-    headers: {
-      authorization: `token ${token}`,
-    },
-  })
-
-  // TODO query in query
-  // не понимаю почему для того, чтобы  увидел мой Email, нужно добавлять его в профиль.
-  // oAuth app вообще не возвращал мне Email
-
-  const response = await graphqlWithAuth(`
-    {
-      viewer { login, name, avatarUrl, email }
-    }
-  `)
-
-  const { viewer } = response
+  const client = octokitGraphqlClient(token)
+  const { viewer } = await client(GET_VIEWER)
   return viewer
 }
