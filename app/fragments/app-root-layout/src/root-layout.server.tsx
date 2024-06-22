0437157058fa206394ed1bfc17dafc85f12dec41
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-22 17:52:11 +0300

    fix(root-layout): remove GH server app

--- app/fragments/app-root-layout/src/root-layout.server.tsx
+++ app/fragments/app-root-layout/src/root-layout.server.tsx
@@ -1,20 +1,7 @@
-import { GithubServerApp }     from '@github-server-app/utils'
-import { getGithubPrivateKey } from '@globals/utils'
-import { checkPrivateKey }     from '@globals/utils'
-import { checkEnvs }           from '@globals/utils'
-
-import fs                      from 'fs'
+import { checkPrivateKey } from '@globals/utils'
+import { checkEnvs }       from '@globals/utils'
 
 export const RootLayoutServer = async () => {
   checkPrivateKey()
   checkEnvs()
-  const GH_PIVATE_KEY = await getGithubPrivateKey()
-  GithubServerApp.authenticate(GH_PIVATE_KEY)
-
-  // const { data } = await GithubServerApp.app.octokit.request('/app')
-  // console.log('authenticated as %s', data.name)
-
-  // for await (const { installation } of GithubServerApp.app.eachInstallation.iterator()) {
-  //   console.log(installation)
-  // }
 }
