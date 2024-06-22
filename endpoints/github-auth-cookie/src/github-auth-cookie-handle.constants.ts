f378ca3f4ad4241fdd230eed23679d1228d8857b
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-22 17:53:56 +0300

    feat(endpoint): add constants

---
+++ endpoints/github-auth-cookie/src/github-auth-cookie-handle.constants.ts
@@ -0,0 +1,1 @@
+export const COOKIE_EXPIRES = 1000 * 60 * 60 * 24 * 7
--- endpoints/github-auth-cookie/src/github-auth-cookie-handle.endpoint.ts
+++ endpoints/github-auth-cookie/src/github-auth-cookie-handle.endpoint.ts
@@ -1,16 +1,19 @@
-import { getGithubAuthToken } from '@globals/utils'
-import { createAppAuth }      from '@octokit/auth-app'
-
 // @ts-ignore:next-line
 import { cookies }            from 'next/headers'
 // @ts-ignore:next-line
 import { redirect }           from 'next/navigation'
 
+import { getGithubAuthToken } from '@globals/utils'
+
+import { COOKIE_EXPIRES }     from './github-auth-cookie-handle.constants.js'
+
 export async function githubAuthCookieHandle(request: Request) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
+
   const token = await getGithubAuthToken(code)
-  cookies().set('token', token)
+  cookies().set('token', token, { expires: Date.now() + COOKIE_EXPIRES })
+
   const redirectUrl = new URL(request.url)
   return redirect(redirectUrl.origin)
 }
