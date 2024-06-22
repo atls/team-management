9c2f5463a116717bbc317f0e3de092bb1977100c
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-22 17:51:49 +0300

    feat(base-layout): redirect on cookie expires

--- app/fragments/app-base-layout/src/base-layout.server.ts
+++ app/fragments/app-base-layout/src/base-layout.server.ts
@@ -1,14 +1,20 @@
+import { cookies }              from 'next/headers'
+import { redirect }             from 'next/navigation'
+
 import { GET_VIEWER }           from '@globals/data'
 import { octokitGraphqlClient } from '@globals/data'
 
-import { cookies }              from 'next/headers'
-
 export const BaseLayoutServer = async () => {
   const token = cookies().get('token').value
-  // TODO redirect to registration if token not exist
-  // TODO middleware
-
-  const client = octokitGraphqlClient(token)
-  const { viewer } = await client(GET_VIEWER)
-  return viewer
+  try {
+    const client = octokitGraphqlClient(token)
+    const { viewer } = await client(GET_VIEWER)
+    return viewer
+  } catch (e) {
+    if (e.status === 401) {
+      redirect('/registration')
+    } else {
+      throw e
+    }
+  }
 }
