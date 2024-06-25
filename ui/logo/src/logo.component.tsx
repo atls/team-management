a77db0a26f1d0f23180ad309df007cc0bb15c466
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-25 12:41:27 +0300

    feat(logo): add logo dard variant

--- ui/logo/src/logo.component.tsx
+++ ui/logo/src/logo.component.tsx
@@ -8,8 +8,29 @@ import { AtlantisSmallIcon } from '@ui/icons'
 import { ThemeType }         from '@ui/theme'
 
 // TODO interfaces
-const Logo: FC<any> = ({ variant }) => {
+const Logo: FC<any> = ({ variant, activeTheme }) => {
   const theme = useTheme() as ThemeType
+
+  if (activeTheme === 'dark') {
+    const LOGO_COLOR = theme.colors.icon
+
+    if (variant === 'small') {
+      return (
+        <AtlantisSmallIcon
+          width={theme.spaces.large}
+          height={theme.spaces.large}
+          color={LOGO_COLOR}
+        />
+      )
+    }
+    return (
+      <AtlantisIcon
+        width={theme.spaces.semiSuperExtra}
+        height={theme.spaces.large}
+        color={LOGO_COLOR}
+      />
+    )
+  }
 
   if (variant === 'small') {
     return <AtlantisSmallIcon width={theme.spaces.large} height={theme.spaces.large} />
