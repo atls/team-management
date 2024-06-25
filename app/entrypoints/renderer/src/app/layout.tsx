87df5f92bee0c9dddd17a809862de6d346c0458d
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-25 13:34:22 +0300

    fix: yarn check fixes

--- app/entrypoints/renderer/src/app/layout.tsx
+++ app/entrypoints/renderer/src/app/layout.tsx
@@ -1,7 +1,10 @@
+import React          from 'react'
+
 import BaseRootLayout from '@app/root-layout'
 
 import messages       from '../../locales/ru.json'
 
+// @ts-expect-error Async Server Component
 const RootLayout = ({ children }) => <BaseRootLayout messages={messages}>{children}</BaseRootLayout>
 
 export default RootLayout
