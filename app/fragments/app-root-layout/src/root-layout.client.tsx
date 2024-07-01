9abb4f45aafb31fd6b0a7952bdf36d761f2fa1dd
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-01 17:49:28 +0300

    refactor: root layout use layout

--- app/fragments/app-root-layout/src/root-layout.client.tsx
+++ app/fragments/app-root-layout/src/root-layout.client.tsx
@@ -1,20 +1,24 @@
 'use client'
 
-import React              from 'react'
-import { IntlProvider }   from 'react-intl'
+import React               from 'react'
+import { IntlProvider }    from 'react-intl'
+import { useLayoutEffect } from 'react'
 
-import { ErrorMessage }   from '@ui/error-message'
-import { ThemeProvider }  from '@ui/theme'
+import { ErrorMessage }    from '@ui/error-message'
+import { ThemeProvider }   from '@ui/theme'
 
-import { checkErrorHook } from './check-error.hook.js'
+import { checkErrorHook }  from './check-error.hook.js'
 
 export const RootLayoutClient = ({ children, messages }) => {
   let errorMessage
   let errorCode
 
-  if (typeof window !== 'undefined') {
-    ;({ errorMessage, errorCode } = checkErrorHook())
-  }
+  useLayoutEffect(() => {
+    if (typeof window !== 'undefined') {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      ;({ errorMessage, errorCode } = checkErrorHook())
+    }
+  }, [])
 
   return (
     <html>
