b0798a797f16a1248027f9d029777f9b16b750cc
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-01 12:46:04 +0300

    refactor(check-envs): change func; moved required vars to consts file

--- globals/environment/src/check-envs.environment.ts
+++ globals/environment/src/check-envs.environment.ts
@@ -1,4 +1,11 @@
+import { REQUIRED_ENV_VAR_NAMES } from './check-envs.constants.js'
+
+const getEnvErrorMessage = (envVar) => {
+  return `need "${envVar}" env variable`
+}
+
 export const checkEnvs = () => {
-  if (!process.env.NEXT_PUBLIC_GH_CLIENT_ID) throw new Error('need github cliend id')
-  if (!process.env.NEXT_PUBLIC_GH_CLIENT_SECRET) throw new Error('need github cliend secret')
+  for (const ENV_VAR_NAME of REQUIRED_ENV_VAR_NAMES) {
+    if (!process.env[ENV_VAR_NAME]) throw new Error(getEnvErrorMessage('NEXT_PUBLIC_GH_CLIENT_ID'))
+  }
 }
