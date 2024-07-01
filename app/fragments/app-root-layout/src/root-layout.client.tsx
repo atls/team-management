bfd5d9fee8daab5fed20812d6fc378d1fae80628
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-01 16:10:28 +0300

    feat(root-layout): render error message; client hook for get error message from queries

--- app/fragments/app-root-layout/src/root-layout.client.tsx
+++ app/fragments/app-root-layout/src/root-layout.client.tsx
@@ -1,16 +1,31 @@
 'use client'
 
-import React             from 'react'
-import { IntlProvider }  from 'react-intl'
+import React              from 'react'
+import { IntlProvider }   from 'react-intl'
 
-import { ThemeProvider } from '@ui/theme'
+import { ErrorMessage }   from '@ui/error-message'
+import { ThemeProvider }  from '@ui/theme'
 
-export const RootLayoutClient = ({ children, messages }) => (
-  <html>
-    <body>
-      <IntlProvider messages={messages} locale='ru' defaultLocale='ru'>
-        <ThemeProvider>{children}</ThemeProvider>
-      </IntlProvider>
-    </body>
-  </html>
-)
+import { checkErrorHook } from './check-error.hook.js'
+
+export const RootLayoutClient = ({ children, messages }) => {
+  let errorMessage
+  let errorCode
+
+  if (typeof window !== 'undefined') {
+    ;({ errorMessage, errorCode } = checkErrorHook())
+  }
+
+  return (
+    <html>
+      <body>
+        <IntlProvider messages={messages} locale='ru' defaultLocale='ru'>
+          <ThemeProvider>
+            <ErrorMessage errorText={errorMessage} errorCode={errorCode} />
+            {children}
+          </ThemeProvider>
+        </IntlProvider>
+      </body>
+    </html>
+  )
+}
