b2c02f1898c2351d75bd8388297437aede02f32a
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-01 16:25:48 +0300

    fix: check env func fix

--- globals/environment/src/check-envs.environment.ts
+++ globals/environment/src/check-envs.environment.ts
@@ -4,6 +4,6 @@ const getEnvErrorMessage = (envVar) => `need "${envVar}" env variable`
 
 export const checkEnvs = () => {
   for (const ENV_VAR_NAME of REQUIRED_ENV_VAR_NAMES) {
-    if (!process.env[ENV_VAR_NAME]) throw new Error(getEnvErrorMessage('NEXT_PUBLIC_GH_CLIENT_ID'))
+    if (!process.env[ENV_VAR_NAME]) throw new Error(getEnvErrorMessage(ENV_VAR_NAME))
   }
 }
