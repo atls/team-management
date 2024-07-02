dfb6517a45a052218c28c06edc1cdd574b2dbb3f
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-02 19:07:06 +0300

    feat(scopes): add org scope

--- app/fragments/app-registration/src/registration.constants.ts
+++ app/fragments/app-registration/src/registration.constants.ts
@@ -1,4 +1,4 @@
 const GH_AUTH_BASE_URL = 'https://github.com/login/oauth/authorize'
 export const GH_AUTH_URL = new URL('', GH_AUTH_BASE_URL)
 
-export const GH_AUTH_SCOPES = 'user:email'
+export const GH_AUTH_SCOPES = 'user:email read:org'
