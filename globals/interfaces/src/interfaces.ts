7506a3028cd18c75ad5a746bdda427e9322c48ed
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-12 20:05:06 +0300

    feat(globals.interfaces): add member data interface

--- globals/interfaces/src/interfaces.ts
+++ globals/interfaces/src/interfaces.ts
@@ -1,8 +1,18 @@
 export type OrganizationDataType = {
   id: number
+  login: string
   name: string
   description: string
   membersWithRole: { totalCount: number }
   avatarUrl: string
   viewerCanAdminister: boolean
+}
+
+export type OrganizationMemberDataType = {
+  id: string
+  login: string
+  url: string
+  name: string
+  bio?: string
+  avatarUrl: string
 }
