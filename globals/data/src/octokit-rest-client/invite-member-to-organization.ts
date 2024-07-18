c4859f3418d373469e17d2be8840dada879a40bb
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-18 19:31:20 +0300

    refactor(rest-client): return promise instead of try catch await

--- globals/data/src/octokit-rest-client/invite-member-to-organization.ts
+++ globals/data/src/octokit-rest-client/invite-member-to-organization.ts
@@ -2,32 +2,18 @@ import { Octokit } from '@octokit/core'
 
 const MEMBER_ROLE = 'direct_member'
 
-export const inviteMemberToOrgaization = async ({
-  token,
-  organizationLogin,
-  githubUserId,
-  teamIds,
-  errorMessageDispatch,
-}) => {
+export const inviteMemberToOrgaization = ({ token, organizationLogin, githubUserId, teamIds }) => {
   const octokit = new Octokit({
     auth: token,
   })
 
-  try {
-    await octokit.request('POST /orgs/{org}/invitations', {
-      org: organizationLogin,
-      invitee_id: githubUserId,
-      role: MEMBER_ROLE,
-      team_ids: teamIds,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    })
-  } catch (e: any) {
-    // eslint-disable-next-line no-console
-    errorMessageDispatch({
-      type: 'set',
-      errorMessage: { text: e.message, code: e.status || 0 },
-    })
-  }
+  return octokit.request('POST /orgs/{org}/invitations', {
+    org: organizationLogin,
+    invitee_id: githubUserId,
+    role: MEMBER_ROLE,
+    team_ids: teamIds,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  })
 }
