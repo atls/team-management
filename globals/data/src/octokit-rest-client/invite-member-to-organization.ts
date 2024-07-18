352d316cbf32d23195b25c96efc2a61b0a9573fb
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-18 14:48:00 +0300

    draft(rest_invite-member): render errors on ui

--- globals/data/src/octokit-rest-client/invite-member-to-organization.ts
+++ globals/data/src/octokit-rest-client/invite-member-to-organization.ts
@@ -7,18 +7,27 @@ export const inviteMemberToOrgaization = async ({
   organizationLogin,
   githubUserId,
   teamIds,
+  errorMessageDispatch,
 }) => {
   const octokit = new Octokit({
     auth: token,
   })
 
-  await octokit.request('POST /orgs/{org}/invitations', {
-    org: organizationLogin,
-    invitee_id: githubUserId,
-    role: MEMBER_ROLE,
-    team_ids: teamIds,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28',
-    },
-  })
+  try {
+    await octokit.request('POST /orgs/{org}/invitations', {
+      org: organizationLogin,
+      invitee_id: githubUserId,
+      role: MEMBER_ROLE,
+      team_ids: teamIds,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    })
+  } catch (e: any) {
+    // eslint-disable-next-line no-console
+    errorMessageDispatch({
+      type: 'set',
+      errorMessage: { text: e.message, code: e.status || 0 },
+    })
+  }
 }
