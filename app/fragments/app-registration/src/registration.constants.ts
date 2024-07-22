b1c9b118ddcb6fb5a8c3c9327fc45586720bff31
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-22 17:42:07 +0300

    fix: remove comment

--- app/fragments/app-registration/src/registration.constants.ts
+++ app/fragments/app-registration/src/registration.constants.ts
@@ -1,5 +1,4 @@
 const GH_AUTH_BASE_URL = 'https://github.com/login/oauth/authorize'
 export const GH_AUTH_URL = new URL('', GH_AUTH_BASE_URL)
 
-// export const GH_AUTH_SCOPES = 'user:email read:org'
 export const GH_AUTH_SCOPES = 'user:email admin:org'
