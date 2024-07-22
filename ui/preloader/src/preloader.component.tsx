a4184169468b95f0a80b080cbacf239fd15255ab
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-22 16:10:43 +0300

    refactor: change format message interface

--- ui/preloader/src/preloader.component.tsx
+++ ui/preloader/src/preloader.component.tsx
@@ -1,10 +1,13 @@
-import React       from 'react'
-import { useIntl } from 'react-intl'
+import React                from 'react'
+import { FormattedMessage } from 'react-intl'
 
-import { Text }    from '@ui/text'
+import { Text }             from '@ui/text'
 
 const Preloader = () => {
-  const { formatMessage } = useIntl()
-  return <Text fontSize='normal.default'>{formatMessage({ id: 'preloader.text' })}</Text>
+  return (
+    <Text fontSize='normal.default'>
+      <FormattedMessage id='preloader.text' />
+    </Text>
+  )
 }
 export { Preloader }
