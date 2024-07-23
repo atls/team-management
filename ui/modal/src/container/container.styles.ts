0d300ffb514b10618910aaf82f040630f7a33d19
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-23 19:38:24 +0300

    fix(modal): zIndex fixes

--- ui/modal/src/container/container.styles.ts
+++ ui/modal/src/container/container.styles.ts
@@ -1,7 +1,7 @@
 import { styleFn } from 'styled-system'
 
 export const baseContainerStyles: styleFn = ({ theme }) => ({
-  zIndex: theme.spaces.s1300,
+  zIndex: theme.spaces.s1500,
   bottom: theme.spaces.zero,
   top: theme.spaces.zero,
   left: theme.spaces.zero,
