551afb147b01f2d26f339a29be4f1c324956de17
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-23 19:36:52 +0300

    refactor(rest-queries): return array against object

--- globals/data/src/rest-queries/remove-organization-member.query.ts
+++ globals/data/src/rest-queries/remove-organization-member.query.ts
@@ -1,12 +1,10 @@
-export const removeOrganizationMemberQuery = async ({ organizationLogin, memberLogin }) => {
-  return [
-    `DELETE /orgs/${organizationLogin}/members/${memberLogin}`,
-    {
-      org: organizationLogin,
-      username: memberLogin,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
+export const removeOrganizationMemberQuery = ({ organizationLogin, memberLogin }) => [
+  `DELETE /orgs/${organizationLogin}/members/${memberLogin}`,
+  {
+    org: organizationLogin,
+    username: memberLogin,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
     },
-  ]
-}
+  },
+]
