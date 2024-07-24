a1c0ae7bb10f3f18694fa348218abbde1c736ab9
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-24 17:25:12 +0300

    refactor: remane add-member-modal to invite-member-modal

--- app/fragments/app-users-title/src/users-title.component.tsx
+++ app/fragments/app-users-title/src/users-title.component.tsx
@@ -1,23 +1,23 @@
-import { useTheme }         from '@emotion/react'
+import { InviteMemberModal } from '@app/invite-member-modal-fragment'
+import { useTheme }          from '@emotion/react'
 
-import React                from 'react'
-import { FormattedMessage } from 'react-intl'
-import { useState }         from 'react'
+import React                 from 'react'
+import { FormattedMessage }  from 'react-intl'
+import { useState }          from 'react'
 
-import { AddMemberModal }   from '@app/add-member-modal-fragment'
-import { Button }           from '@ui/button'
-import { AddIcon }          from '@ui/icons'
-import { Row }              from '@ui/layout'
-import { Text }             from '@ui/text'
-import { ThemeType }        from '@ui/theme'
+import { Button }            from '@ui/button'
+import { AddIcon }           from '@ui/icons'
+import { Row }               from '@ui/layout'
+import { Text }              from '@ui/text'
+import { ThemeType }         from '@ui/theme'
 
 const UsersTitle: React.FC = () => {
   const theme = useTheme() as ThemeType
 
-  const [isAddMemberModalOpen, setAddMemberModalOpen] = useState(false)
+  const [isInviteMemberModalOpen, setInviteMemberModalOpen] = useState(false)
 
   const handlerPlusButtonClick = () => {
-    setAddMemberModalOpen(!isAddMemberModalOpen)
+    setInviteMemberModalOpen(!isInviteMemberModalOpen)
   }
 
   return (
@@ -38,7 +38,7 @@ const UsersTitle: React.FC = () => {
           color={theme.colors.text.primary}
         />
       </Button>
-      <AddMemberModal open={isAddMemberModalOpen} onBackdropClick={handlerPlusButtonClick} />
+      <InviteMemberModal open={isInviteMemberModalOpen} onBackdropClick={handlerPlusButtonClick} />
     </Row>
   )
 }
