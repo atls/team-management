109c6616bc5a0861473b354786a7365449bffea1
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-25 12:17:54 +0300

    refactor(invite-button): interfaces; sintax

--- app/fragments/app-invite-button/src/invite-button.constants.ts
+++ app/fragments/app-invite-button/src/invite-button.constants.ts
@@ -1,8 +1,6 @@
 import { ThemeType } from '@ui/theme'
 
-export const getInviteButtonConstants = (theme: ThemeType) => {
-  return {
-    SUCCESS_COLOR: theme.colors.BLUE_400,
-    ICON_SIZE: theme.spaces.semiRegular,
-  }
-}
+export const getInviteButtonConstants = (theme: ThemeType) => ({
+  SUCCESS_COLOR: theme.colors.BLUE_400,
+  ICON_SIZE: theme.spaces.semiRegular,
+})
