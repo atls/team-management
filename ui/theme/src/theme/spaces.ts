a1b9c2e41d54bf47e7f2558df675df79c5a5ab0d
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-26 19:26:52 +0300

    feat(theme/spaces): add params

--- ui/theme/src/theme/spaces.ts
+++ ui/theme/src/theme/spaces.ts
@@ -113,13 +113,14 @@ export const spaces = {
   t24r24b0l0: '24px 24px 0 0',
   v8h16: '8px 16px',
   v8h12: '8px 12px',
+  t8h16b16: '8px 16px 16px',
   v0h24: '0px 24px',
   mediumDefault: 50,
   mediumIncreased: 80,
 
   largeReduced: 93,
   largeSemiReduced: 100,
-  largeSemiNormal: 139,
+  largeSemiNormal: 140,
   largeSemiNormalIncreased: 160,
   largeSemi: 185,
 
