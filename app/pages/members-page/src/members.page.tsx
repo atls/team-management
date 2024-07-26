4252c05411f323690aa7cd01485863cf08eb7139
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-26 19:24:49 +0300

    refactor(members-page): move all login and data functions to fragment; remove interfaces; create query constants(limits)

--- app/pages/members-page/src/members.page.tsx
+++ app/pages/members-page/src/members.page.tsx
@@ -1,8 +1,16 @@
-import { MemberCardsGrid } from '@app/member-cards-grid'
+import React                          from 'react'
+import { FC }                         from 'react'
 
-import React               from 'react'
-import { FC }              from 'react'
+import { MemberCardsGrid }            from '@app/member-cards-grid'
 
-const MembersPage: FC = () => <MemberCardsGrid />
+import { ORGANIZATIONS_LIMIT }        from './members.constants.js'
+import { ORGANIZATION_MEMBERS_LIMIT } from './members.constants.js'
+
+const MembersPage: FC = () => (
+  <MemberCardsGrid
+    organizationsLimit={ORGANIZATIONS_LIMIT}
+    organizationMembersLimit={ORGANIZATION_MEMBERS_LIMIT}
+  />
+)
 
 export default MembersPage
