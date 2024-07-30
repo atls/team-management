46192858f59414fd493ebc3e10657db3850be760
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-30 15:02:07 +0300

    refactor(progrss): absolute to text container on progress

--- ui/progress/src/progress.component.tsx
+++ ui/progress/src/progress.component.tsx
@@ -5,6 +5,7 @@ import { useTheme }           from '@emotion/react'
 import React                  from 'react'
 import { FC }                 from 'react'
 
+import { Condition }          from '@ui/condition'
 import { Box }                from '@ui/layout'
 import { Text }               from '@ui/text'
 import { ThemeType }          from '@ui/theme'
@@ -13,17 +14,25 @@ export const Progress: FC<ProgressProps> = ({ percentage }) => {
   const theme = useTheme() as ThemeType
 
   return (
-    <Box width='100%' height={theme.spaces.medium} backgroundColor={theme.colors.BLUE_0}>
+    <Box
+      width='100%'
+      height={theme.spaces.medium}
+      backgroundColor={theme.colors.BLUE_0}
+      position='relative'
+    >
       <Box
         width={`${percentage}%`}
         height='100%'
-        padding={theme.spaces.v0h24}
         alignItems='center'
         backgroundColor={theme.colors.BLUE_400}
       >
-        <Text fontSize='small.default' color={theme.colors.white}>
-          {percentage}% done
-        </Text>
+        <Condition match={Boolean(percentage)}>
+          <Box padding={theme.spaces.v0h24} position='absolute' width='100%'>
+            <Text fontSize='small.default' color={theme.colors.white}>
+              {percentage}% done
+            </Text>
+          </Box>
+        </Condition>
       </Box>
     </Box>
   )
