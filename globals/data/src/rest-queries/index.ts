416b5b8e8f6b10506a3051a7780f81ff6b5e1eec
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-30 15:46:00 +0300

    feat(queries): add gh2fa query

--- globals/data/src/rest-queries/index.ts
+++ globals/data/src/rest-queries/index.ts
@@ -1,3 +1,4 @@
 export * from './invite-member-to-organization.query.js'
 export * from './invite-member-to-organization-email.query.js'
 export * from './remove-organization-member.query.js'
+export * from './get-github-organization-members-without-2fa.query.js'
