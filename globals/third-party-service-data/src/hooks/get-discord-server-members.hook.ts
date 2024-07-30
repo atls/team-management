afbf2280cacc096fef56b87fa0492536157af71f
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-30 19:20:40 +0300

    refactor(globals): refactor

--- globals/third-party-service-data/src/hooks/get-discord-server-members.hook.ts
+++ globals/third-party-service-data/src/hooks/get-discord-server-members.hook.ts
@@ -6,18 +6,21 @@ export const getDiscordServerMembers = async () => {
   const API_TOKEN = process.env.NEXT_PUBLIC_DISCORD_API_TOKEN as string
   const GUILD_ID = process.env.NEXT_PUBLIC_DISCORD_GUILD_ID as string
 
-  console.log(API_TOKEN)
-  console.log(GUILD_ID)
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-  console.log(Client)
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+  })
 
+  client.login(API_TOKEN)
+
+  // console.log(API_TOKEN)
+  //
   // const client = new Client({
   //   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
   //   partials: [Partials.GuildMember],
   // })
-
-  console.log(client)
-
+  //
   // client.once('ready', async () => {
   //   console.log(`Мы вошли как ${client.user?.tag}`)
   //
@@ -33,5 +36,13 @@ export const getDiscordServerMembers = async () => {
   //   client.destroy()
   // })
   //
-  // client.login(API_TOKEN)
+  // console.log('\n')
+  // console.log('after client once')
+  // console.log('\n')
+
+  client.login(API_TOKEN)
+
+  console.log('\n')
+  console.log('after client login')
+  console.log('\n')
 }
--- globals/third-party-service-data/src/hooks/get-figma-collaborators.hook.ts
+++ globals/third-party-service-data/src/hooks/get-figma-collaborators.hook.ts
@@ -1,16 +1,12 @@
-export const getFigmaTeamCollaborators = async (teamId) => {
-  const requestUrl = new URL(`https://api.figma.com/v1/teams/${teamId}/projects`)
-  const response = await fetch(requestUrl.href, {
-    headers: {
-      'X-Figma-Token': figmaAccessToken,
-    },
-  })
-  const responseData = await response.json()
-  console.log(responseData)
-  return responseData
-  // TODO должны приходить collaborators
-}
-//
-// const TEAM_ID = '1180582603387288055'
-// const teamId = TEAM_ID
-// const figmaCollaborators = await getFigmaTeamCollaborators(teamId)
+// export const getFigmaTeamCollaborators = async (teamId) => {
+//   const requestUrl = new URL(`https://api.figma.com/v1/teams/${teamId}/projects`)
+//   const response = await fetch(requestUrl.href, {
+//     headers: {
+//       'X-Figma-Token': figmaAccessToken,
+//     },
+//   })
+//   const responseData = await response.json()
+//   console.log(responseData)
+//   return responseData
+//   // TODO должны приходить collaborators
+// }
