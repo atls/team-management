a99cad8951a06300f1a5a03be97a8da21c1166bb
Viktoria Dragun <aliceinapple99@gmail.com>
2025-01-31 18:13:03 +0300

    feat(mobile): add app screens

---
+++ mobile/app/src/screens/organizations/organizations.screen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const OrganizationsScreen = () => {
+  return (
+    <View>
+      <Text>Organizations</Text>
+    </View>
+  );
+};
+
