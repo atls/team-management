dc4573cd01f114e1e6f0afd602412e7d22525e8e
Viktoria Dragun <aliceinapple99@gmail.com>
2025-02-10 18:54:31 +0300

    chore(mobile): run formatting

--- mobile/app/app/app/index.tsx
+++ mobile/app/app/app/index.tsx
@@ -1,13 +1,15 @@
-import {Button} from 'react-native'
-import { useRouter } from 'expo-router';
+import React         from 'react'
+import { Button }    from 'react-native'
+import { useRouter } from 'expo-router'
 
-export default function App() {
-  const {push} = useRouter();
+const App = () => {
+  const { push } = useRouter()
   return (
     <>
-      <Button title='go to sign-in' onPress={()=>push('/sign-in')}/>
-      <Button title='go to dashboard' onPress={()=>push('/dashboard')}/>
+      <Button title='go to sign-in' onPress={() => push('/sign-in')} />
+      <Button title='go to dashboard' onPress={() => push('/dashboard')} />
     </>
-  );
+  )
 }
 
+export default App
--- mobile/app/app/metro.config.js
+++ mobile/app/app/metro.config.js
@@ -1,25 +1,21 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config')
+const path = require('path')
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
 config.resolver.extraNodeModules = {
   ...config.resolver.extraNodeModules,
   stream: require.resolve('stream-browserify'),
   buffer: require.resolve('buffer'),
-};
-
+}
 
 config.resolver.nodeModulesPaths = [
   path.resolve(__dirname, '../node_modules'),
   path.resolve(__dirname, 'node_modules'),
   path.resolve(__dirname, '../screens/node_modules'),
   path.resolve(__dirname, '../ui/node_modules'),
-];
-
-config.watchFolders = [
-  path.resolve(__dirname, '../screens'),
-  path.resolve(__dirname, '../ui'),
 ]
 
-module.exports = config;
+config.watchFolders = [path.resolve(__dirname, '../screens'), path.resolve(__dirname, '../ui')]
+
+module.exports = config
