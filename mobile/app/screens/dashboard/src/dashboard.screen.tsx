dcd793b9d926f77768fdf7e1de8abca1cdc5e386
Viktoria Dragun <aliceinapple99@gmail.com>
2025-02-12 10:14:40 +0300

    refactor(mobile): structure

--- mobile/app/screens/dashboard/src/dashboard.screen.tsx
+++
@@ -1,9 +0,0 @@
-import React    from 'react'
-import { Text } from 'react-native'
-import { View } from 'react-native'
-
-export const DashboardScreen = () => (
-  <View>
-    <Text>Dashboard</Text>
-  </View>
-)
