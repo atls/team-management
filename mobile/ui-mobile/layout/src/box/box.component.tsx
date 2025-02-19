44ccb0005e0f3ca5bafaf5c2ab3f73f54fd63821
Viktoria Dragun <aliceinapple99@gmail.com>
2025-02-19 10:17:13 +0300

    refactor(common): move ui-mobile to project root

--- mobile/ui-mobile/layout/src/box/box.component.tsx
+++
@@ -1,8 +0,0 @@
-import React          from 'react'
-
-import { BoxElement } from './box.element'
-import { BoxProps }   from './box.interfaces'
-
-export const Box = ({ children, ...props }: BoxProps) => (
-  <BoxElement {...props}>{children}</BoxElement>
-)
