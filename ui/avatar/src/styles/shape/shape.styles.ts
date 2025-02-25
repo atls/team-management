f78d7d321d6d8dab7261ddd77fb486ddc4aa0ede
Viktoria Dragun <aliceinapple99@gmail.com>
2025-02-25 20:31:33 +0300

    fix(ui): avatar lint errors

--- ui/avatar/src/styles/shape/shape.styles.ts
+++ ui/avatar/src/styles/shape/shape.styles.ts
@@ -1,8 +1,15 @@
+import type { styleFn }           from 'styled-system'
+
 import type { AvatarShapeStyles } from './shape.interfaces.js'
 
 import { createShapeStyles }      from '@atls-ui-parts/avatar'
 
-export const getShapeStyles = ({ size, borderWidth, padding, borderRadius }: AvatarShapeStyles) =>
+export const getShapeStyles = ({
+  size,
+  borderWidth,
+  padding,
+  borderRadius,
+}: AvatarShapeStyles): styleFn =>
   createShapeStyles({
     size: size ?? 0,
     borderWidth: borderWidth ?? 0,
