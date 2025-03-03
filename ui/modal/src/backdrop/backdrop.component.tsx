b1d78f352054fcbfa57b34ffb2460392967461fc
Viktoria Dragun <aliceinapple99@gmail.com>
2025-03-03 12:46:21 +0300

    fix(ui): modal lint errors

--- ui/modal/src/backdrop/backdrop.component.tsx
+++ ui/modal/src/backdrop/backdrop.component.tsx
@@ -1,16 +1,18 @@
 import styled                     from '@emotion/styled'
+import React                      from 'react'
 
 import { BackdropElement }        from './backdrop.element.js'
 import { backdropMotionVariants } from './backdrop.motion.js'
 import { baseBackdropStyles }     from './backdrop.styles.js'
 
-const Backdrop = styled(BackdropElement)(baseBackdropStyles)
+const BaseBackdrop = styled(BackdropElement)(baseBackdropStyles)
 
-Backdrop.defaultProps = {
-  variants: backdropMotionVariants,
-  animate: 'target',
-  initial: 'enter',
-  exit: 'exit',
-}
+const Backdrop: React.FC<React.ComponentProps<typeof BaseBackdrop>> = ({
+  variants = backdropMotionVariants,
+  animate = 'target',
+  initial = 'enter',
+  exit = 'exit',
+  ...props
+}) => <BaseBackdrop {...{ variants, animate, initial, exit, ...props }} />
 
 export { Backdrop }
