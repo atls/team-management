9143b5cd5778951a2136d419223676985c421adc
Viktoria Dragun <aliceinapple99@gmail.com>
2025-03-03 12:48:19 +0300

    fix(ui): preloader lint errors

--- ui/preloader/src/preloader.component.tsx
+++ ui/preloader/src/preloader.component.tsx
@@ -1,9 +1,11 @@
+import type { FC }          from 'react'
+
 import { FormattedMessage } from 'react-intl'
 import React                from 'react'
 
 import { Text }             from '@ui/text'
 
-const Preloader = () => (
+const Preloader: FC = () => (
   <Text fontSize='normal.default'>
     <FormattedMessage id='preloader.text' />
   </Text>
